db6c12c4ff764afa440143fc2d1163ce8399585e
kkopitehong <kkopite.hong@smartcorelink.com>
2022-03-28 16:00:24 +0800

    chore: add demo

---
+++ playground/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import unocss from 'unocss/vite'
+import { presetAttributify, presetUno } from 'unocss'
+import { presetScrollbar } from '../src'
+
+export default defineConfig({
+  plugins: [
+    unocss({
+      presets: [
+        presetUno(),
+        presetAttributify(),
+        presetScrollbar(),
+      ],
+    }),
+  ],
+})
